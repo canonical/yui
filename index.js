77955aea4fcfa596d9e8a65960d4e9af737b34a0
Goulin Khoge <contact+git@goulin.fr>
2026-06-29 13:45:15 +0200

    fix: add CommonJS entry point
    Package main pointed at a missing ./index.js; require('@canonical/yui') failed. Add index.js re-exporting the Node seed so { YUI } resolves.

---
+++ index.js
@@ -0,0 +1,3 @@
+// CommonJS entry point. Mirrors the historical `yui` npm package: requiring the
+// package yields the Node seed which exports `{ YUI }`.
+module.exports = require('./build/yui-nodejs/yui-nodejs.js');
